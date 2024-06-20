192151d32abd9c5da6aa4818c1eeba8242f29f9a
Alex Sinitsin <meowcoder@gmail.com>
2024-06-20 22:26:13 +0500

    Add an option to bundle tesseract

--- build.rs
+++ build.rs
@@ -6,6 +6,40 @@ use std::path::PathBuf;
 #[cfg(windows)]
 use vcpkg;
 
+fn compile_tesseract() -> Vec<String> {
+    let lept = std::env::var("DEP_LEPT_ROOT").expect("no DEP_LEPT_ROOT env variable");
+
+    let dst = cmake::Config::new("tesseract")
+        .define("Leptonica_DIR", format!("{lept}/lib/cmake/leptonica"))
+        .define("CMAKE_BUILD_TYPE", "Release")
+        .define("SW_BUILD", "OFF")
+        .define("BUILD_TRAINING_TOOLS", "OFF")
+        .define("OPENMP_BUILD", "OFF")
+        .define("USE_SYSTEM_ICU", "OFF")
+        .define("ENABLE_LTO", "OFF")
+        .define("ENABLE_NATIVE", "OFF")
+        .define("DISABLE_ARCHIVE", "ON")
+        .define("DISABLE_CURL", "ON")
+        .define("DISABLE_TIFF", "ON")
+        .define("DISABLED_LEGACY_ENGINE", "OFF")
+        .define("GRAPHICS_DISABLED", "OFF")
+        // avoids error in leptonica, due to missing Tiff support
+        .cxxflag("-DTESSERACT_DISABLE_DEBUG_FONTS=1")
+        // https://github.com/tesseract-ocr/tesseract/pull/2965
+        .cxxflag("-DTESSERACT_IMAGEDATA_AS_PIX=1")
+        .build();
+
+    std::env::set_var("PKG_CONFIG_PATH", format!("{lept}/lib/pkgconfig:{}/lib/pkgconfig", dst.display()));
+
+    let pk = pkg_config::Config::new().probe("tesseract").unwrap();
+    let _ = std::fs::hard_link(dst.join(format!("lib/lib{}.a", pk.libs[0])), dst.join("lib/libtesseract.a"));
+
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    println!("cargo:rustc-link-lib=static=tesseract");
+
+    vec![dst.join("include").display().to_string()]
+}
+
 #[cfg(windows)]
 fn find_tesseract_system_lib() -> Vec<String> {
     println!("cargo:rerun-if-env-changed=TESSERACT_INCLUDE_PATHS");
@@ -131,7 +165,14 @@ fn public_types_bindings(_clang_extra_include: &[String]) -> &'static str {
 fn main() {
     // Tell cargo to tell rustc to link the system tesseract
     // and leptonica shared libraries.
-    let clang_extra_include = find_tesseract_system_lib();
+
+    println!("cargo:rerun-if-env-changed=TESSERACT_BUNDLE");
+
+    let clang_extra_include = if std::env::var_os("TESSERACT_BUNDLE").is_some() {
+        compile_tesseract()
+    } else {
+        find_tesseract_system_lib()
+    };
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
